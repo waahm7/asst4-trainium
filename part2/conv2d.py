ebbf4de32db9de2eac20caa5424979bb0dee892a
Waqar Ahmed Khan <waahm7@gmail.com>
2025-06-20 19:51:02 -0700

    slow but works with partition tiling

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -61,8 +61,8 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     assert nl.tile_size.gemm_moving_fmax >= out_width
 
     # Various tiling dimensions (You may want to define more of them)
-    c_in_pmax = nl.tile_size.pmax
-    n_tiles_c_in = in_channels // c_in_pmax
+    c_in_pmax = nl.tile_size.pmax # max partition dimension (128)
+    n_tiles_c_in = in_channels // c_in_pmax # this will should be the tiling loop which is 256/128
 
     # Initialize output array
     X_out = nl.ndarray(
@@ -78,52 +78,29 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     
     # Process the images in batches
     for batch in nl.affine_range(batch_size): # For Each image
-        #x_tile = nl.load(X_re[batch])
-        #nl.device_print("value of first Reshape:"+str(X_re.shape)+":", x_tile)
-        # ERROR: The type of fancy compilation needed is not working with psum which is
-        # output_3d[:, y, x] += output[:, 0]
-        output_3d = nl.zeros((out_channels, out_height, out_width), dtype=X.dtype, buffer=nl.psum)
-        #nl.device_print("\tvalue of output 2d:"+str(output_2d.shape)+":",output_2d)
-        for i in nl.affine_range(filter_height):
-            for j in nl.affine_range(filter_width):
-                filter = nl.load(W[i,j])
-                #nl.device_print("\t---------value of filter:-----"+str(filter.shape)+":",filter) 
-                # old working but stupid code
-                # for y in nl.affine_range(out_height):
-                #     for x in nl.affine_range(out_width):
-                #         in_y = y + i
-                #         in_x = x + j
-                #         input_pos = in_y * input_width + in_x
-                #         # Layout is [channels x 1]
-                #         input_slice = nl.load(X_re[batch, :, input_pos])
-                #         #nl.device_print("\tvalue of input slice:"+str(input_slice.shape)+":",input_slice)
-                #         output = nl.matmul(filter, input_slice, transpose_x = True)
-                #         # Shape of output is channels x 1
-                #         output_3d[:, y, x] += output[:, 0]
-                #         nl.device_print("\tvalue of output:"+str(output.shape)+":",output)
-                # How to build one matrix to multiply
+        for tile in nl.affine_range(n_tiles_c_in):
+            start_idx = tile*c_in_pmax
+            end_idx = tile*c_in_pmax + c_in_pmax
+            output_3d = nl.zeros((c_in_pmax, out_height, out_width), dtype=X.dtype, buffer=nl.psum)
+            for i in nl.affine_range(filter_height):
+                for j in nl.affine_range(filter_width):
+                    # I think this needs tiling, as input channels needs to be summed up to both
+                     for tile_inner in nl.affine_range(n_tiles_c_in):
+                        start_idx_inner = tile_inner*c_in_pmax
+                        end_idx_inner = tile_inner*c_in_pmax + c_in_pmax
+                        filter = nl.load(W[i,j,start_idx_inner:end_idx_inner,start_idx:end_idx])
+                        im2col = nl.ndarray((c_in_pmax, out_height, out_width), dtype=X.dtype, buffer=nl.sbuf)
+                        for y in nl.affine_range(out_height):
+                            for x in nl.affine_range(out_width):
+                                in_y = y + i
+                                in_x = x + j
+                                input_pos = in_y * input_width + in_x 
+                                im2col[:,y,x] = nl.load(X_re[batch,start_idx_inner:end_idx_inner,input_pos])
+                        output_3d += nl.matmul(filter, im2col, transpose_x = True)
 
-                im2col = nl.ndarray((in_channels, filter_height, filter_width), dtype=X.dtype, buffer=nl.sbuf)
-                for y in nl.affine_range(out_height):
-                    for x in nl.affine_range(out_width):
-                        in_y = y + i
-                        in_x = x + j
-                        input_pos = in_y * input_width + in_x 
-                        im2col[:,y,x] = nl.load(X_re[batch,:,input_pos])
-                output_3d += nl.matmul(filter, im2col, transpose_x = True)
-
-                       
-        # Need to copy from psum to sbuf before we can copy it to HBM
-        result_sbuf = nl.copy(output_3d, dtype=X.dtype)
-        # #nl.device_print("\tvalue of final result:"+str(result_sbuf.shape)+":",result_sbuf)
-        nl.store(X_out[batch], value=result_sbuf)
-
-          # Copy to SBUF with proper shape and type
-       
-
-    # Just copy one element to demonstrate
-    # temp = nl.load(X[0, 0, 0, 0])  # Load a single value
-    #nl.store(X_out[0, 0, 0, 0], temp)
-    
+            # Need to copy from psum to sbuf before we can copy it to HBM
+            result_sbuf = nl.copy(output_3d, dtype=X.dtype)
+            #nl.device_print("\tvalue of final result:"+str(result_sbuf.shape)+":",result_sbuf)
+            nl.store(X_out[batch][start_idx:end_idx], value=result_sbuf)
     return X_out
 
